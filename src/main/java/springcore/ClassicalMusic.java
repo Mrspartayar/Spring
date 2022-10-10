a836c9c89cd178d5ad56cc2cd8ed96b0908de0ef
Yaroslav <yaroslav_lav1@bk.ru>
2022-10-10 17:19:33 +0300

    Lesson10

--- src/main/java/springcore/ClassicalMusic.java
+++ src/main/java/springcore/ClassicalMusic.java
@@ -1,20 +1,10 @@
 package springcore;
 
-public class ClassicalMusic implements Music{
-    private ClassicalMusic() {}
-
-    public static ClassicalMusic getClassicalMusic() {
-        return new ClassicalMusic();
-    }
-
-    public void doMyInit(){
-        System.out.println("Doing my initialization");
-    }
-    public void doMyDestroy(){
-        System.out.println("Doing my destruction");
-    }
+import org.springframework.stereotype.Component;
+@Component
+public class ClassicalMusic implements Music {
     @Override
-    public String getSong(){
+    public String getSong() {
         return "Hungrian";
     }
 }
--- src/main/java/springcore/TestSpring.java
+++ src/main/java/springcore/TestSpring.java
@@ -6,8 +6,20 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        ClassicalMusic ClassicalMusic = context.getBean("musicBean",ClassicalMusic.class);
-        System.out.println(ClassicalMusic.getSong());
+//        Music music = context.getBean("rockMusic",Music.class);
+//        MusicPlayer musicPlayer = new MusicPlayer(music);
+//        musicPlayer.playMusic();
+//
+//        Music music2 = context.getBean("classicalMusic",Music.class);
+//        MusicPlayer musicPlayer2 = new MusicPlayer(music2);
+//        musicPlayer2.playMusic();
+
+    //    MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+    //    musicPlayer.playMusic();
+
+        Computer computer = context.getBean("computer", Computer.class);
+        System.out.println(computer);
+        context.close();
 
 
 //        Music music = context.getBean("musicBean", Music.class);
@@ -29,6 +41,5 @@ public class TestSpring {
 //        System.out.println(musicPlayer.getName());
 //        System.out.println(musicPlayer.getVolume());
 
-        context.close();
     }
 }
