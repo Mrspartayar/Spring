2ca51ecbed8a5f56a8a2f63bd5c171b5f3118099
Yaroslav <yaroslav_lav1@bk.ru>
2022-10-11 16:17:54 +0300

    Lesson12

--- src/main/java/springcore/ClassicalMusic.java
+++ src/main/java/springcore/ClassicalMusic.java
@@ -1,8 +1,20 @@
 package springcore;
 
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
 public class ClassicalMusic implements Music {
+    @PostConstruct
+    public void doMyInit() {
+        System.out.println("Doing my init");
+    }
+    @PreDestroy
+    public void doMyDestroy() {
+        System.out.println("Doing my destruction");
+    }
     @Override
     public String getSong() {
         return "Hungrian";
